44a7c660b5d31f20fa67b101507694ed065c5170
xooooooooox <xozoz.sos@gmail.com>
2026-02-06 13:33:44 +0800

    fix: completion

--- src/main/shell/commands/completion.sh
+++ src/main/shell/commands/completion.sh
@@ -2,6 +2,7 @@
 # @cmd
 # @desc Generate shell completion script
 # @arg shell! Shell type (bash or zsh)
+# @arg-values shell bash zsh
 # @example completion bash > ~/.local/share/bash-completion/completions/homelabctl
 # @example completion zsh > ~/.zfunc/_homelabctl
 
